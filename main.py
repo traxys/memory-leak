c249782403c82388388e28f85d55f7b1ba7ab96a
AurelienMoisson <>
2019-01-25 01:39:49 +0100

    tabs are evil

--- main.py
+++ main.py
@@ -6,34 +6,34 @@ import verygen
 
 
 def menu(scr, choices, title = None):
-	scr.nodelay(False)
-	selected = 0
-	while True:
-		scr.clear()
-		height, width = scr.getmaxyx()
-		if title != None:
-			start_l = (height-len(choices)+1)//2
-			scr.addstr(start_l-1, (width-len(title))//2, title)
-		else:
-			start_l = (height-len(choices))//2
-		for l in range(len(choices)):
-			txt = choices[l]
-			start_c = (width-len(txt))//2
-			if l == selected:
-				scr.addstr(start_l+l, start_c, txt, curses.A_REVERSE)
-			else:
-				scr.addstr(start_l+l, start_c, txt)
-		scr.refresh()
-		k = scr.getkey()
-		if k=="KEY_DOWN":
-			selected += 1
-		elif k=="KEY_UP":
-			selected -= 1
-		elif k=="\n" or k==" ":
-			scr.clear()
-			scr.nodelay(True)
-			return selected
-		selected %= len(choices)
+    scr.nodelay(False)
+    selected = 0
+    while True:
+        scr.clear()
+        height, width = scr.getmaxyx()
+        if title != None:
+            start_l = (height-len(choices)+1)//2
+            scr.addstr(start_l-1, (width-len(title))//2, title)
+        else:
+            start_l = (height-len(choices))//2
+        for l in range(len(choices)):
+            txt = choices[l]
+            start_c = (width-len(txt))//2
+            if l == selected:
+                scr.addstr(start_l+l, start_c, txt, curses.A_REVERSE)
+            else:
+                scr.addstr(start_l+l, start_c, txt)
+        scr.refresh()
+        k = scr.getkey()
+        if k=="KEY_DOWN":
+            selected += 1
+        elif k=="KEY_UP":
+            selected -= 1
+        elif k=="\n" or k==" ":
+            scr.clear()
+            scr.nodelay(True)
+            return selected
+        selected %= len(choices)
 
 
 def main(scr):
