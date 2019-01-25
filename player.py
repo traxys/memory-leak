16c7b968f5f9d97985c72422382bc89b153a88ea
Quentin Boyer <qbsecond@gmail.com>
2019-01-25 03:06:09 +0100

    player level to room

--- player.py
+++ player.py
@@ -7,11 +7,11 @@ class Bomb:
         self.x = x
         self.y = y
         self.duration = duration
-        self.level = current_level
+        self.room = current_level
         self.damage = damage
 
     def explode(self):
-        for tile in self.level.current_level.tonari_janai(self.x, self.y):
+        for tile in self.room.current_level.tonari_janai(self.x, self.y):
             tile.damage(self.damage)
 
     def update(self):
@@ -22,9 +22,9 @@ class Bomb:
 
 class Player:
     def __init__(self,current_level):
-        self.level = 1
+        self.room = 1
         self.hpbonus = 0
-        self.level = current_level
+        self.room = current_level
         self.bomb_strength = 2
         self.bomb_duration = 10
         self.x = 0
@@ -34,26 +34,26 @@ class Player:
         self.direction = utils.Direction.Kita
         
     def get_hp_max(self):
-        return (self.level * 100) + (self.hpbonus)
+        return (self.room * 100) + (self.hpbonus)
 
     def move(self, direction):
-        if direction == utils.Direction.Higashi and not self.level.is_a_wall(self.x, self.y):
-            self.level.grid[self.x][self.y] = None
+        if direction == utils.Direction.Higashi and not self.room.is_a_wall(self.x, self.y):
+            self.room.grid[self.x][self.y] = None
             self.x += 1
-        if direction == utils.Direction.Minami and not self.level.is_a_wall(self.x, self.y):
-            self.level.grid[self.x][self.y] = None
+        if direction == utils.Direction.Minami and not self.room.is_a_wall(self.x, self.y):
+            self.room.grid[self.x][self.y] = None
             self.y += 1
-        if direction == utils.Direction.Nishi and not self.level.is_a_wall(self.x, self.y):
-            self.level.grid[self.x][self.y] = None
+        if direction == utils.Direction.Nishi and not self.room.is_a_wall(self.x, self.y):
+            self.room.grid[self.x][self.y] = None
             self.x -= 1
-        if direction == utils.Direction.Kita and not self.level.is_a_wall(self.x, self.y):
-            self.level.grid[self.x][self.y] = None
+        if direction == utils.Direction.Kita and not self.room.is_a_wall(self.x, self.y):
+            self.room.grid[self.x][self.y] = None
             self.y -= 1
-        self.level.grid[self.x][self.y] = self
-        if self.level.grid[self.x][self.y].is_a_gate():
-            x = self.level.grid[self.x][self.y].next_x
-            y = self.level.grid[self.x][self.y].next_y
-            self.level = self.level.grid[self.x][self.y].next_room
+        self.room.grid[self.x][self.y] = self
+        if self.room.grid[self.x][self.y].is_a_gate():
+            x = self.room.grid[self.x][self.y].next_x
+            y = self.room.grid[self.x][self.y].next_y
+            self.room = self.room.grid[self.x][self.y].next_room
             self.x = x
             self.y = y
 
@@ -67,5 +67,5 @@ class Player:
             self.items.append(item)
 
     def put_bomb(self):
-        bomb = Bomb(self.x, self.y, self.bomb_strength, self.bomb_duration, self.level)
+        bomb = Bomb(self.x, self.y, self.bomb_strength, self.bomb_duration, self.room)
 
